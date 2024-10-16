3dba27d508c18cb04d3e40cc20adfe79579926e5
Victor <vhduartelima@hotmail.com>
2024-10-15 23:29:22 -0300

    avancando no InsertCustomerUseCaseImpl

---
+++ src/main/java/com/victorhugo/cleanarch/core/dataprovider/InsertCustomer.java
@@ -0,0 +1,7 @@
+package com.victorhugo.cleanarch.core.dataprovider;
+
+import com.victorhugo.cleanarch.core.domain.Customer;
+
+public interface InsertCustomer {
+    void insert(Customer customer);
+}
--- src/main/java/com/victorhugo/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
+++ src/main/java/com/victorhugo/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
@@ -1,6 +1,7 @@
 package com.victorhugo.cleanarch.core.usecase.impl;
 
 import com.victorhugo.cleanarch.core.dataprovider.FindAddressByZipCode;
+import com.victorhugo.cleanarch.core.dataprovider.InsertCustomer;
 import com.victorhugo.cleanarch.core.domain.Address;
 import com.victorhugo.cleanarch.core.domain.Customer;
 import com.victorhugo.cleanarch.core.usecase.InsertCustomerUseCase;
@@ -8,9 +9,17 @@ import com.victorhugo.cleanarch.core.usecase.InsertCustomerUseCase;
 public class InsertCustomerUseCaseImpl implements InsertCustomerUseCase {
 
     private final FindAddressByZipCode findAddressByZipCode;
+    private final InsertCustomer insertCustomer;
+
+    public InsertCustomerUseCaseImpl(FindAddressByZipCode findAddressByZipCode, InsertCustomer insertCustomer) {
+        this.findAddressByZipCode = findAddressByZipCode;
+        this.insertCustomer = insertCustomer;
+    }
 
     @Override
     public void insert(Customer customer, String zipCode) {
         Address address = findAddressByZipCode.find(zipCode);
+        customer.setAddress(address);
+        insertCustomer.insert(customer);
     }
 }
