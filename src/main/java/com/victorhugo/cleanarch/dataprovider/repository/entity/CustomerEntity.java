633a346fb7833b90eb6cdd889b5383105e659ef8
Victor Hugo <victor.duarte@groupsoftware.com.br>
2024-10-18 21:09:43 -0300

    added entity

---
+++ src/main/java/com/victorhugo/cleanarch/dataprovider/repository/entity/CustomerEntity.java
@@ -0,0 +1,21 @@
+package com.victorhugo.cleanarch.dataprovider.repository.entity;
+
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+
+@Document(collation = "customers")
+@Data
+public class CustomerEntity {
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String cpf;
+    private AddressEntity addressEntity;
+    private String password;
+    private LocalDate lastSession;
+    private Boolean isValidCpf;
+    private boolean deleted = Boolean.FALSE;
+}
