c1bb73828d738a5fa548dcf597850a1d88db6755
Victor <vhduartelima@hotmail.com>
2024-10-21 02:29:54 -0300

    atualizado InsertCustomerRequest

--- src/main/java/com/victorhugo/cleanarch/entrypoint/controller/request/InsertCustomerRequest.java
+++ src/main/java/com/victorhugo/cleanarch/entrypoint/controller/request/InsertCustomerRequest.java
@@ -1,11 +1,13 @@
 package com.victorhugo.cleanarch.entrypoint.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class InsertCustomerRequest {
     @NotBlank
     private String name;
     @NotBlank private String email;
     @NotBlank private String cpf;
-    @NotBlank private String password;
+    @NotBlank private String zipCode;
 }
