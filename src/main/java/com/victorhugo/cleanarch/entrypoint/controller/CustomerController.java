26c084c3b8f46dd8b6114faea5875a5c8338d29b
Victor <vhduartelima@hotmail.com>
2024-10-21 02:31:13 -0300

    ajustado controller e método insert

--- src/main/java/com/victorhugo/cleanarch/entrypoint/controller/CustomerController.java
+++ src/main/java/com/victorhugo/cleanarch/entrypoint/controller/CustomerController.java
@@ -1,16 +1,29 @@
 package com.victorhugo.cleanarch.entrypoint.controller;
 
+import com.victorhugo.cleanarch.core.usecase.InsertCustomerUseCase;
+import com.victorhugo.cleanarch.entrypoint.controller.mapper.CustomerMapper;
 import com.victorhugo.cleanarch.entrypoint.controller.request.InsertCustomerRequest;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+    private final InsertCustomerUseCase insertCustomerUseCase;
+    private final CustomerMapper customerMapper;
+    public CustomerController(InsertCustomerUseCase insertCustomerUseCase, CustomerMapper customerMapper) {
+        this.insertCustomerUseCase = insertCustomerUseCase;
+        this.customerMapper = customerMapper;
+    }
+
     @PostMapping
-    public ResponseEntity<Void> insert(InsertCustomerRequest insertCustomerRequest){
+    public ResponseEntity<Void> insert(@Valid @RequestBody InsertCustomerRequest insertCustomerRequest){
+        var customer = customerMapper.toCustomer(insertCustomerRequest);
+        insertCustomerUseCase.insert(customer, insertCustomerRequest.getZipCode());
         return ResponseEntity.ok().build();
     }
 }
