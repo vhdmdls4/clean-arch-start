f3073130362f8619827366f0914af2eedffd9c97
Victor <vhduartelima@hotmail.com>
2024-10-20 23:56:05 -0300

    adicionado InsertCustomerRequest

--- src/main/java/com/victorhugo/cleanarch/entrypoint/controller/request/InsertCustomerRequest.java
+++ src/main/java/com/victorhugo/cleanarch/entrypoint/controller/request/InsertCustomerRequest.java
@@ -1,4 +1,11 @@
 package com.victorhugo.cleanarch.entrypoint.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class InsertCustomerRequest {
+    @NotBlank
+    private String name;
+    @NotBlank private String email;
+    @NotBlank private String cpf;
+    @NotBlank private String password;
 }
