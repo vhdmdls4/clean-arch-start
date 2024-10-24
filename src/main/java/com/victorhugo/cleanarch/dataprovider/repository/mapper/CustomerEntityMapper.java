9319752614636aa511117978e387022811a9e2b3
Victor <vhduartelima@hotmail.com>
2024-10-23 23:09:48 -0300

    mapeamento toCustomer criado no CustomerEntityMapper

--- src/main/java/com/victorhugo/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
+++ src/main/java/com/victorhugo/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
@@ -7,4 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface CustomerEntityMapper {
     CustomerEntity toCustomerEntity(Customer customer);
+    Customer toCustomer(CustomerEntity customerEntity);
 }
