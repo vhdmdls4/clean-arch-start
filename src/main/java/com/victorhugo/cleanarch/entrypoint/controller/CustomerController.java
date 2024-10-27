f46fadae523921a7901d4d454910f9fed339ca66
Victor <vhduartelima@hotmail.com>
2024-10-26 23:54:15 -0300

    adicionado método findById para o controller de customer

--- src/main/java/com/victorhugo/cleanarch/entrypoint/controller/CustomerController.java
+++ src/main/java/com/victorhugo/cleanarch/entrypoint/controller/CustomerController.java
@@ -1,23 +1,25 @@
 package com.victorhugo.cleanarch.entrypoint.controller;
 
+import com.victorhugo.cleanarch.core.domain.Customer;
+import com.victorhugo.cleanarch.core.usecase.FindCustomerByIdUseCase;
 import com.victorhugo.cleanarch.core.usecase.InsertCustomerUseCase;
 import com.victorhugo.cleanarch.entrypoint.controller.mapper.CustomerMapper;
 import com.victorhugo.cleanarch.entrypoint.controller.request.InsertCustomerRequest;
+import com.victorhugo.cleanarch.entrypoint.controller.response.CustomerResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
     private final InsertCustomerUseCase insertCustomerUseCase;
     private final CustomerMapper customerMapper;
-    public CustomerController(InsertCustomerUseCase insertCustomerUseCase, CustomerMapper customerMapper) {
+    private final FindCustomerByIdUseCase findCustomerByIdUseCase;
+    public CustomerController(InsertCustomerUseCase insertCustomerUseCase, CustomerMapper customerMapper, FindCustomerByIdUseCase findCustomerByIdUseCase) {
         this.insertCustomerUseCase = insertCustomerUseCase;
         this.customerMapper = customerMapper;
+        this.findCustomerByIdUseCase = findCustomerByIdUseCase;
     }
 
     @PostMapping
@@ -25,5 +27,12 @@ public class CustomerController {
         var customer = customerMapper.toCustomer(insertCustomerRequest);
         insertCustomerUseCase.insert(customer, insertCustomerRequest.getZipCode());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
+        Customer customer = findCustomerByIdUseCase.find(id);
+        CustomerResponse customerResponse = customerMapper.toCustomerResponse(customer);
+        return ResponseEntity.ok(customerResponse);
     }
 }
