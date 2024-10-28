38de24a588bc6d6775fcb5717535db8b22a78c9a
Victor <vhduartelima@hotmail.com>
2024-10-27 21:45:19 -0300

    adicionado SendCpfForValidation no InsertCustomerUseCaseImpl

--- src/main/java/com/victorhugo/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
+++ src/main/java/com/victorhugo/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
@@ -2,20 +2,21 @@ package com.victorhugo.cleanarch.core.usecase.impl;
 
 import com.victorhugo.cleanarch.core.dataprovider.FindAddressByZipCode;
 import com.victorhugo.cleanarch.core.dataprovider.InsertCustomer;
+import com.victorhugo.cleanarch.core.dataprovider.SendCpfForValidation;
 import com.victorhugo.cleanarch.core.domain.Address;
 import com.victorhugo.cleanarch.core.domain.Customer;
 import com.victorhugo.cleanarch.core.usecase.InsertCustomerUseCase;
-import org.springframework.stereotype.Component;
 
-@Component
 public class InsertCustomerUseCaseImpl implements InsertCustomerUseCase {
 
     private final FindAddressByZipCode findAddressByZipCode;
     private final InsertCustomer insertCustomer;
+    private final SendCpfForValidation sendCpfForValidation;
 
-    public InsertCustomerUseCaseImpl(FindAddressByZipCode findAddressByZipCode, InsertCustomer insertCustomer) {
+    public InsertCustomerUseCaseImpl(FindAddressByZipCode findAddressByZipCode, InsertCustomer insertCustomer, SendCpfForValidation sendCpfForValidation) {
         this.findAddressByZipCode = findAddressByZipCode;
         this.insertCustomer = insertCustomer;
+        this.sendCpfForValidation = sendCpfForValidation;
     }
 
     @Override
