acd23e585ceee37b26bebd5ed56ec60eac58f9f3
Victor <vhduartelima@hotmail.com>
2024-10-27 21:43:59 -0300

    adicionado lombok constructor

--- src/main/java/com/victorhugo/cleanarch/dataprovider/FindCustomerByIdImpl.java
+++ src/main/java/com/victorhugo/cleanarch/dataprovider/FindCustomerByIdImpl.java
@@ -5,19 +5,16 @@ import com.victorhugo.cleanarch.core.domain.Customer;
 import com.victorhugo.cleanarch.dataprovider.repository.CustomerRepository;
 import com.victorhugo.cleanarch.dataprovider.repository.entity.CustomerEntity;
 import com.victorhugo.cleanarch.dataprovider.repository.mapper.CustomerEntityMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Component
 public class FindCustomerByIdImpl implements FindCustomerById {
     private final CustomerRepository customerRepository;
     private final CustomerEntityMapper customerEntityMapper;
-
-    public FindCustomerByIdImpl(CustomerRepository customerRepository, CustomerEntityMapper customerEntityMapper) {
-        this.customerRepository = customerRepository;
-        this.customerEntityMapper = customerEntityMapper;
-    }
 
     @Override
     public Optional<Customer> find(final Long id) {
