a018cfe75b79f0a907827226de5139abd0284f58
Victor <vhduartelima@hotmail.com>
2024-10-27 21:42:58 -0300

    adicionado @repository para o CustomerRepository

--- src/main/java/com/victorhugo/cleanarch/dataprovider/repository/CustomerRepository.java
+++ src/main/java/com/victorhugo/cleanarch/dataprovider/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.victorhugo.cleanarch.dataprovider.repository;
 
 import com.victorhugo.cleanarch.dataprovider.repository.entity.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<CustomerEntity, Long> {
 }
