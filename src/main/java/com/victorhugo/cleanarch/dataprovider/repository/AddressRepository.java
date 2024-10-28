ee270c282527cb151a1799d864dcc68640b8a47a
Victor <vhduartelima@hotmail.com>
2024-10-27 21:42:14 -0300

    adicionado nomenclatura de component especifico para o spring

--- src/main/java/com/victorhugo/cleanarch/dataprovider/repository/AddressRepository.java
+++ src/main/java/com/victorhugo/cleanarch/dataprovider/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.victorhugo.cleanarch.dataprovider.repository;
 
 import com.victorhugo.cleanarch.dataprovider.repository.entity.AddressEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends MongoRepository<AddressEntity, String> {
 }
